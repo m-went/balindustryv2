11f3006e4acd8bf861a435dc27fa0471ff653e13
Martyna Filipiak <fyrodems@gmail.com>
2024-02-28 15:00:58 +0100

    lang selector refactor

--- src/components/LanguageSelector/LanguageSelector.tsx
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -5,14 +5,58 @@ import { useLocale } from 'next-intl'
 import Image from 'next/image'
 import { Link, usePathname } from '@/navigation'
 
+const SupportedLocales = ['pl', 'en', 'de', 'fr'] as const
+
+type Locale = (typeof SupportedLocales)[number]
+
 interface LangWithFlagProps {
-  locale: 'en' | 'pl' | 'de' | 'fr' | undefined
+  locale: Locale | undefined
 }
 
-export const LanguageSelector: React.FC = () => {
+const generateLanguageName = (locale: Locale | undefined): string => {
+  switch (locale) {
+    case 'pl': {
+      return 'Polski'
+    }
+
+    case 'en': {
+      return 'English'
+    }
+
+    case 'de': {
+      return 'Deutsch'
+    }
+
+    case 'fr': {
+      return 'Français'
+    }
+
+    default: {
+      return ''
+    }
+  }
+}
+
+const LinkWithFlag: React.FC<LangWithFlagProps & { pathname: string }> = ({
+  locale,
+  pathname,
+}) => (
+  <Link href={pathname} locale={locale}>
+    <Image
+      src={`https://hatscripts.github.io/circle-flags/flags/${locale === 'en' ? 'gb' : locale}.svg`}
+      alt={'flaga'}
+      height={25}
+      width={25}
+    />
+    {generateLanguageName(locale)}
+  </Link>
+)
+
+const GenerateLanguageSelectorTrigger: React.FC<LangWithFlagProps> = ({
+  locale,
+}) => {
   const [isOpen, setIsOpen] = useState(false)
   const languageRef = useRef<HTMLDivElement>(null)
-  const locale = useLocale() as 'en' | 'pl' | 'de' | 'fr' | undefined
   const pathname = usePathname()
 
   useEffect(() => {
@@ -31,79 +75,38 @@ export const LanguageSelector: React.FC = () => {
     }
   }, [])
 
-  const generateLanguageName = (
-    locale: 'en' | 'pl' | 'de' | 'fr' | undefined
-  ) => {
-    switch (locale) {
-      case 'pl': {
-        return 'Polski'
-      }
-
-      case 'en': {
-        return 'English'
-      }
-
-      case 'de': {
-        return 'Deutsch'
-      }
-
-      case 'fr': {
-        return 'Français'
-      }
-
-      default: {
-        return ''
-      }
-    }
-  }
-
-  const GenerateLanguageSelectorTrigger: React.FC<{
-    locale: 'en' | 'pl' | 'de' | 'fr' | undefined
-  }> = ({ locale }) => (
-    <>
-      <div
-        onClick={() => {
-          setIsOpen(!isOpen)
-        }}
-      >
-        <Image
-          src={`https://hatscripts.github.io/circle-flags/flags/${
-            locale === 'en' ? 'gb' : locale
-          }.svg`}
-          alt={'flaga'}
-          height={25}
-          width={25}
-        />
-        {generateLanguageName(locale)}
-      </div>
-    </>
-  )
-
-  const LinkWithFlag: React.FC<LangWithFlagProps> = ({ locale }) => (
-    <Link href={pathname} locale={locale}>
+  return (
+    <div
+      ref={languageRef}
+      className="language-selector"
+      onClick={() => {
+        setIsOpen(!isOpen)
+      }}
+    >
       <Image
-        src={`https://hatscripts.github.io/circle-flags/flags/${
-          locale === 'en' ? 'gb' : locale
-        }.svg`}
+        src={`https://hatscripts.github.io/circle-flags/flags/${locale === 'en' ? 'gb' : locale}.svg`}
         alt={'flaga'}
         height={25}
         width={25}
       />
       {generateLanguageName(locale)}
-    </Link>
-  )
-
-  return (
-    <div ref={languageRef} className="language-selector">
-      <GenerateLanguageSelectorTrigger locale={locale} />
       {isOpen && (
         <div className="language-options">
-          <LinkWithFlag locale="pl" />
-          <LinkWithFlag locale="en" />
-          <LinkWithFlag locale="de" />
-          <LinkWithFlag locale="fr" />
+          {SupportedLocales.map((supportedLocale) => (
+            <LinkWithFlag
+              key={supportedLocale}
+              locale={supportedLocale}
+              pathname={pathname}
+            />
+          ))}
         </div>
       )}
     </div>
   )
+}
+
+export const LanguageSelector: React.FC = () => {
+  const locale = useLocale() as Locale | undefined
+
+  return <GenerateLanguageSelectorTrigger locale={locale} />
 }
