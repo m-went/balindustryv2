4656629ae19672eb48dc3a8c6d31459d19e232ed
Michal <m.went@kratki.com>
2024-02-29 14:15:46 +0100

    add more styles to popularOffers slider

--- src/components/homePage/PopularOffers/PopularOffers.tsx
+++ src/components/homePage/PopularOffers/PopularOffers.tsx
@@ -3,13 +3,67 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/pagination'
-import { Pagination } from 'swiper/modules'
-import styles from './PopularOffers.module.scss'
-import photo from '../../../../public/products/twin_one_axis.png'
+import { Pagination, Navigation, Autoplay } from 'swiper/modules'
 import Image from 'next/image'
+import { useRef, useCallback } from 'react'
+import photo from '../../../../public/products/twin_one_axis.png'
+import styles from './PopularOffers.module.scss'
+import useWindowDimensions from '@/hooks/useWindowDimensions'
+import { FaArrowDown } from 'react-icons/fa6'
 
 export default function PopularOffers() {
+  const windowDimensions = useWindowDimensions()
+  const sliderRef = useRef(null)
+
   const slidesContent = [
+    {
+      img: photo,
+      productName: 'Stanowisko Twin One-axis',
+      productSecondLine: 'Robot Gofa 15000, UR5/UR12',
+      lowestPrice: 'Najniższa cena z ostatnich 30 dni: 7,50 zł',
+      actualPrice: '300$',
+      lastPrice: '350$',
+    },
+    {
+      img: photo,
+      productName: 'Stanowisko Twin One-axis22',
+      productSecondLine: 'Robot Gofa 15000, UR5/UR12',
+      lowestPrice: 'Najniższa cena z ostatnich 30 dni: 7,50 zł',
+      actualPrice: '300$',
+      lastPrice: '350$',
+    },
+    {
+      img: photo,
+      productName: 'Stanowisko Twin One-axis',
+      productSecondLine: 'Robot Gofa 15000, UR5/UR12',
+      lowestPrice: 'Najniższa cena z ostatnich 30 dni: 7,50 zł',
+      actualPrice: '300$',
+      lastPrice: '350$',
+    },
+    {
+      img: photo,
+      productName: 'Stanowisko Twin One-axis',
+      productSecondLine: 'Robot Gofa 15000, UR5/UR12',
+      lowestPrice: 'Najniższa cena z ostatnich 30 dni: 7,50 zł',
+      actualPrice: '300$',
+      lastPrice: '350$',
+    },
+    {
+      img: photo,
+      productName: 'Stanowisko Twin One-axis',
+      productSecondLine: 'Robot Gofa 15000, UR5/UR12',
+      lowestPrice: 'Najniższa cena z ostatnich 30 dni: 7,50 zł',
+      actualPrice: '300$',
+      lastPrice: '350$',
+    },
+    {
+      img: photo,
+      productName: 'Stanowisko Twin One-axis',
+      productSecondLine: 'Robot Gofa 15000, UR5/UR12',
+      lowestPrice: 'Najniższa cena z ostatnich 30 dni: 7,50 zł',
+      actualPrice: '300$',
+      lastPrice: '350$',
+    },
     {
       img: photo,
       productName: 'Stanowisko Twin One-axis',
@@ -36,50 +90,63 @@ export default function PopularOffers() {
     },
   ]
 
-  const slidesPagination = {
-    clickable: true,
-    renderBullet(index: any, className: string) {
-      return (
-        '<div class="' +
-        className +
-        ' ' +
-        styles.paginationBullet +
-        '"><span></span></div>'
-      )
-    },
-  }
+  const swipePrev = useCallback(() => {
+    if (!sliderRef.current) return
+    sliderRef.current.swiper.slidePrev()
+  }, [])
+
+  const swipeNext = useCallback(() => {
+    if (!sliderRef.current) return
+    sliderRef.current.swiper.slideNext()
+  }, [])
 
   return (
-    <div>
+    <div className={styles.popularOffers}>
       <h2>Popularne oferty</h2>
       <Swiper
-        pagination={slidesPagination}
-        modules={[Pagination]}
+        ref={sliderRef}
+        pagination={false}
+        modules={[Pagination, Navigation]}
         loop={true}
-        className="mySwiper"
+        className={styles.swiper}
+        slidesPerView={Math.floor(windowDimensions.width / 320)}
+        spaceBetween={40}
+        navigation={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        speed={2000}
       >
         {slidesContent.map((slide, index) => (
-          <SwiperSlide key={index}>
+          <SwiperSlide key={index} className={styles.swiperSlide}>
             <div className={styles.slideContainer}>
               <Image src={slide.img} alt="Product photo" />
               <h3>{slide.productName}</h3>
-              <p>{slide.productSecondLine}</p>
-              <p>{slide.lowestPrice}</p>
-              <div>
-                {slide.actualPrice}{' '}
-                <p
-                  style={{
-                    textDecorationLine: 'line-through',
-                    textDecorationStyle: 'solid',
-                  }}
-                >
-                  {slide.lastPrice}
-                </p>
+              <p className={styles.productSecondLine}>
+                {slide.productSecondLine}
+              </p>
+              <div className={styles.actualPrice}>
+                <p>{slide.actualPrice}</p>
+                <p>{slide.lastPrice}</p>
               </div>
+              <p className={styles.lowestPrice}>{slide.lowestPrice}</p>
             </div>
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className={styles.buttonsContainer}>
+        <div>
+          <button onClick={() => swipePrev()} className={styles.swipeButton}>
+            <FaArrowDown />
+          </button>
+        </div>
+        <div>
+          <button onClick={() => swipeNext()} className={styles.swipeButton}>
+            <FaArrowDown />
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
