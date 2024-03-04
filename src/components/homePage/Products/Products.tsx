2526bed4f80ff2a17cfe65f43ae6de40bbcae1a0
Michal <m.went@kratki.com>
2024-03-04 07:57:44 +0100

    add resposive views for mainPage products

--- src/components/homePage/Products/Products.tsx
+++ src/components/homePage/Products/Products.tsx
@@ -10,35 +10,39 @@ export default function Products() {
     {
       topHeader: 'Robotyzacja i automatyzacja',
       topPhoto: cela,
+      topParagraph: null,
+      topButton: 'Formularz',
       bottomHeader: 'Robotyzacja i automatyzacja',
       bottomText:
         'Poznaj ofertę produktową skierowaną do przedsiębiorstw każdej wielkości',
-      topParagraph: null,
     },
     {
       topHeader: 'Park maszynowy',
       topPhoto: waterjet,
+      topParagraph: null,
+      topButton: 'Formularz',
       bottomHeader: 'Park maszynowy',
       bottomText:
         'Sprawdź sprzęt, na którym pracujemy i dowiedz się, dlaczego jesteśmy najlepsi',
-      topParagraph: null,
     },
     {
       topHeader: 'Rozwiązania',
       topPhoto: magazyn,
+      topParagraph: null,
+      topButton: 'Formularz',
       bottomHeader: 'Rozwiązania',
       bottomText:
         'Jesteś ciekaw, co sprawia, że nasi klienci darzą nas takim zaufaniem? Przyjrzyj się rozwiązaniom, z których korzystamy',
-      topParagraph: null,
     },
     {
       topHeader: 'Usługi',
       topPhoto: null,
+      topParagraph:
+        'Sprawdź nasze usługi. Wypełnij formularz ofertowy na naszej stronie.',
+      topButton: 'Formularz',
       bottomHeader: 'Usługi',
       bottomText:
         'Chcesz nawiązać współpracę? Skorzystaj z naszego kalkulatora albo wypełnij formularz kontaktowy.',
-      topParagraph:
-        'Sprawdź nasze usługi. Wypełnij formularz ofertowy na naszej stronie.',
     },
   ]
 
@@ -50,10 +54,11 @@ export default function Products() {
           <div key={card.topHeader} className={styles.card}>
             <div className={styles.cardTop}>
               {card.topParagraph === null && card.topPhoto !== null ? (
-                <>
+                <div>
                   <h3>{card.topHeader}</h3>
                   <Image src={card.topPhoto} alt="Section photo" />
-                </>
+                  <Button disabled={false} content={card.topButton} size="XL" />
+                </div>
               ) : (
                 <div className={styles.servicesCard}>
                   <div className={styles.headerWOImg}>
